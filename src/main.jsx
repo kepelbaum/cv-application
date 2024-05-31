5b8c5d966b320fd3ab81dbd3432d6268c6d37f2e
Kirill Epelbaum <150293425+kepelbaum@users.noreply.github.com>
2024-05-31 13:53:29 -0400

    feat: basic template set up

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Test from './test.jsx';
+import './styles.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Test />
+  </React.StrictMode>,
+)
---
+++ src/test.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+function Test() {
+    // const [count, setCount] = useState(0)
+    // <h1 style={{color: 'red'}}>Hello World!</h1>
+  
+    return (
+        <div className='main'>
+            <div className='input'>
+                <h1>Input</h1>
+            </div>
+            <div className='output'>
+                <h1>Output</h1>
+            </div>
+        </div>
+    )
+  }
+  
+  export default Test;
